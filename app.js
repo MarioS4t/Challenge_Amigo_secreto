7784443d87443be8c28cdaaafafc9ddfa3e60e92
Mario Martínez Serrano <108465048+MarioS4t@users.noreply.github.com>
2025-03-12 21:34:34 -0600

    Update app.js
     función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+// Funcion para agregar amigos
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+    input.focus();
+}
